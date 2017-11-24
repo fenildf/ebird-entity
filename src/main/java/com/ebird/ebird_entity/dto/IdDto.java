015a2b8f8bb84681dc8f5ae046b00e33b807f11f
wlz <wanglizong@meizu.com>
2017-11-24 13:49:29 +0800

    修改ID返回类

---
+++ src/main/java/com/ebird/ebird_entity/dto/IdDto.java
@@ -0,0 +1,28 @@
+package com.ebird.ebird_entity.dto;
+
+import java.util.Date;
+
+public class IdDto {
+	
+	public Integer id ;
+	
+	public Date createTime;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	
+
+}
